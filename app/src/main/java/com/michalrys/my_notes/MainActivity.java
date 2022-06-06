5a39af5d43c59f77864e37b5634fdf962c744202
michalrys <michalrys@gmail.com>
2022-06-06 20:34:32 +0200

    lab1: second activity show after button press.

--- app/src/main/java/com/michalrys/my_notes/MainActivity.java
+++ app/src/main/java/com/michalrys/my_notes/MainActivity.java
@@ -2,13 +2,25 @@ package com.michalrys.my_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.michalrys.my_notes.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
