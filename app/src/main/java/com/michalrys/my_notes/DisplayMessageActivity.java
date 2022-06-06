5f9a8877f8a035f1c8c1cd6fdd52dc77ef69212e
michalrys <michalrys@gmail.com>
2022-06-06 20:39:04 +0200

    lab1: message send to second activity and is shown

--- app/src/main/java/com/michalrys/my_notes/DisplayMessageActivity.java
+++ app/src/main/java/com/michalrys/my_notes/DisplayMessageActivity.java
@@ -2,13 +2,26 @@ package com.michalrys.my_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // get message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //create text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //set text view as the activity layout
+        setContentView(textView);
     }
 }
