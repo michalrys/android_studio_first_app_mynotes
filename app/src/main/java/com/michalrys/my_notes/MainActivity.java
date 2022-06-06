2755a4d72712af5c28ffe8c35b5d4c72b71a3538
michalrys <michalrys@gmail.com>
2022-06-06 21:28:13 +0200

    lab1: simple counter is working now.

--- app/src/main/java/com/michalrys/my_notes/MainActivity.java
+++ app/src/main/java/com/michalrys/my_notes/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.michalrys.my_notes.MESSAGE";
@@ -21,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void counter(View view) {
+        Intent intent = new Intent(this, DisplayCounter.class);
         startActivity(intent);
     }
 }
