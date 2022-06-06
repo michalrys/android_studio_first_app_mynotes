328b740e40458a08966a8f89607b48faed22cd89
michalrys <michalrys@gmail.com>
2022-06-06 22:45:14 +0200

    my notes: view for new note.

--- app/src/main/java/com/michalrys/my_notes/MyNotesMainActivity.java
+++ app/src/main/java/com/michalrys/my_notes/MyNotesMainActivity.java
@@ -2,7 +2,9 @@ package com.michalrys.my_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyNotesMainActivity extends AppCompatActivity {
 
@@ -16,5 +18,10 @@ public class MyNotesMainActivity extends AppCompatActivity {
         //TODO: when edit button is pressed --> open activity like for sending message and place
         //TODO:     there title, content, save button and cancel button.
 
+    }
+
+    public void createNewNote(View view) {
+        Intent intent = new Intent(this, MyNotesDisplayNewNoteActivity.class);
+        startActivity(intent);
     }
 }
