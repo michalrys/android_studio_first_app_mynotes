08f361f6f7817b2f5ef0b070eedcf11e3dca27a2
michalrys <michalrys@gmail.com>
2022-06-07 20:12:04 +0200

    create Note and DataBaseManager based on lab2.

--- app/src/main/java/com/michalrys/my_notes/MyNotesMainActivity.java
+++ app/src/main/java/com/michalrys/my_notes/MyNotesMainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class MyNotesMainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MyNotesMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes_main);
+        ListView myNotes = findViewById(R.id.mynotes_list);
 
         //TODO: create some dummy input for db
         //TODO: load notes and create list with delete button and edit button
